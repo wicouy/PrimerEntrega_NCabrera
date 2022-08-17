70ce4c010ed97bed975e8ea897f6b01488bc8d11
wicouy <wico123@gmail.com>
2022-08-17 14:58:18 +0200

    first commit

---
+++ src/components/CustomLoader.js
@@ -0,0 +1,8 @@
+const CustomLoader = () => {
+    return (
+        <div class="logoHDP"><img src={'loading.png'}/> 
+        <h1>...ESTOY CARGANDO Bolu... espera</h1>
+        </div>
+    )
+}
+export default CustomLoader
